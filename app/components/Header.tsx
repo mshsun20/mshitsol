3d8ad2e406e53d551af3c0f6401f725f7d5a9960
mrigankashiv <mriganka.sekhar@shyamgroup.com>
2025-12-01 04:14:12 +0530

    about page inclusion

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,23 +4,17 @@ import React from 'react'
 const menuItems = [
   { text: "Home", href: "/" },
   { text: "About Us", href: "/about/" },
-  { text: "Product", href: "/products/" },
-  { text: "CSR", href: "/sustainability-csr/" },
-  { text: "Media", href: "/brand-marketing-media-coverage/" },
-  { text: "Contact Us", href: "/contact-us/" },
   {
-    text: "Factories",
-    href: "/factories/",
+    text: "Projects",
+    href: "/projects/",
     children: [
-      { text: "Jamuria", href: "/factories/jamuria/" },
-      { text: "Sambhalpur", href: "/factories/sambhalpur/" },
+      { text: "Live", href: "/projects/live/" },
+      { text: "Under Development", href: "/projects/underdev/" },
     ],
   },
-  { text: "Quality & Credentials", href: "/quality-testing/" },
-  { text: "Projects", href: "/projects/" },
   { text: "Testimonials", href: "/testimonials/" },
   { text: "Blog", href: "/blog/difference-between-tie-beam-and-plinth-beam" },
-  { text: "TMT Bars Rate Chart", href: "/tmt-bars-rate-chart/" },
+  { text: "Contact Us", href: "/contact-us/" },
   { text: "FAQ", href: "/faq/" },
 ];
 
@@ -29,11 +23,7 @@ const Header = () => {
     <div className='header'>
         <div className='logosec'>MSHIT-Sol</div>
         <div className="navsec">
-            {
-                menuItems?.map(elm => (
-                    <Link key={elm?.text} href={elm?.href} className='navlink'>{elm?.text}</Link>
-                ))
-            }
+            { menuItems?.map(elm => <Link key={elm?.text} href={elm?.href} className='navlink'>{elm?.text}</Link>) }
         </div>
     </div>
   )
