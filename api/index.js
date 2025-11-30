d6824ad2c6592f9a648a9ff3b36d72ef7e1b8bff
mrigankashiv <mriganka.sekhar@shyamgroup.com>
2025-11-30 17:29:59 +0530

    api codebase converted into serverless

--- api/index.js
+++ api/index.js
@@ -5,64 +5,63 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import serverless from "serverless-http";
-import mongoose from "mongoose";
-
+import mongoConn from './db/dbcon.js';
 import authRoutes from "./routes/authRoute.js";
 import routes from "./routes/route.js";
 
 dotenv.config({ quiet: true });
 
+const apienv = process.env.NODE_ENV || 'dev';
+const appenv = process.env.APP_ENV || 'quality';
+
+const portDetails = {
+  quality: process.env.PORT_QAS || 5502,
+  production: process.env.PORT_PRD || 5501,
+}
+const port = portDetails[appenv] || 5502;
+
+const allowedOrigins = {
+  dev: {
+    quality: [process.env.APP_URL_DEVQ],
+    production: [process.env.APP_URL_DEVP]
+  },
+  live: {
+    quality: [process.env.APP_URL_LIVQ],
+    production: [process.env.APP_URL_LIVP]
+  }
+}
+const origins = allowedOrigins[apienv][appenv] || ['http://localhost:3502'];
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+
 
 const app = express();
 
 
 // ------------------ MIDDLEWARES ------------------
+
+app.set("trust proxy", "loopback");
 app.use(express.json());
 app.use(cookieParser());
-app.set("trust proxy", true);
-
-const allowedOrigins = [
-  process.env.APP_URL_DEVQ,
-  process.env.APP_URL_DEVP,
-  process.env.APP_URL_LIVQ,
-  process.env.APP_URL_LIVP,
-].filter(Boolean);
-
-app.use(
-  cors({
-    origin: allowedOrigins,
+app.use(cors({
+    origin: origins,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept', 'X-Requested-With', 'Authorization'],
     credentials: true,
-  })
-);
-
+    optionsSuccessStatus: 200
+}));
 app.use(bodyParser.json({ limit: '10000mb' }));
 app.use(bodyParser.urlencoded({ limit: '10000mb', extended: true }));
+app.use(express.static('uploads'));
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-// ------------------ ROUTES ------------------
-app.use("/auth", authRoutes);
-app.use("/", routes);
+app.use('/auth', authRoutes);
+app.use('/', routes);
 
 // Health check
+await mongoConn();
 
 // ------------------ EXPORT ------------------
 export default app;
-
-// import express from 'express';
-
-// const app = express();
-
-// app.get('/', (req, res) => {
-//   res.json({ message: 'API working from Vercel serverless!' });
-// });
-
-// app.get('/hello', (req, res) => {
-//   res.json({ message: 'Hello from Express on Vercel!' });
-// });
-
-// export default app;
-
