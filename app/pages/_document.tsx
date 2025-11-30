103d6ff7bd92ffe124e517243c13c4c0fe0bc74e
mrigankashiv <mriganka.sekhar@shyamgroup.com>
2025-12-01 01:02:11 +0530

    SEO Optimisation done using GSC and sitemap and robots.txt added in public folder

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-site-verification" content="S5gfUIo2NvD0RtbYQMoHnCUrOFKvZTVAjCgVhnlPAUw" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://mshitsol-app.vercel.app/" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
