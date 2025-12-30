a9e0e696e1499b629ad2d2592ef126aff9aeefc8
mrigankashiv <mriganka.sekhar@shyamgroup.com>
2025-12-31 03:34:54 +0530

    counter section design dev done

--- app/components/layouts/CounterSection/CounterPage.tsx
+++ app/components/layouts/CounterSection/CounterPage.tsx
@@ -28,25 +28,25 @@ const CounterPage = () => {
         <div className='counter-sec'>
             <div className="counter-card">
                 <div className="counter-icon"><Briefcase className='icn' /></div>
-                <div className="counter-value">{livep}</div>
+                <div className="counter-value">{`${livep}+`}</div>
                 <div className="counter-hds">Projects Completed</div>
                 <div className="counter-details">Several industrial software projects delivered</div>
             </div>
             <div className="counter-card">
                 <div className="counter-icon"><CodeXml className='icn' /></div>
-                <div className="counter-value">{devp}</div>
+                <div className="counter-value">{`${devp}+`}</div>
                 <div className="counter-hds">In Development</div>
                 <div className="counter-details">Active projects currently being built</div>
             </div>
             <div className="counter-card">
                 <div className="counter-icon"><Calendar className='icn' /></div>
-                <div className="counter-value">{yrOfExp}</div>
+                <div className="counter-value">{`${yrOfExp}+`}</div>
                 <div className="counter-hds">Years Experience</div>
                 <div className="counter-details">Professional development experience</div>
             </div>
             <div className="counter-card">
                 <div className="counter-icon"><Layers className='icn' /></div>
-                <div className="counter-value">{skillCnt}</div>
+                <div className="counter-value">{`${skillCnt}+`}</div>
                 <div className="counter-hds">Teck Stacks</div>
                 <div className="counter-details">Several Technologies and frameworks mastered</div>
             </div>
